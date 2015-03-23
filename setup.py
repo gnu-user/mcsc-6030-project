a95ded935b8ec10473c8d35e3018e66aec21e915
gnu_user <gsoc.student@gmail.com>
2015-03-23 12:03:06 -0400

    Added a project setup file

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+
+def read(fname):
+    with open(fname) as fp:
+        content = fp.read()
+    return content
+
+
+setup(
+    name='Enhancement of Matrix Multiplication',
+    version="0.0.1",
+    description='''
+    Research project to evaluate strategies for the enhancement of matrix
+    multiplcation using parallelization and evolutionary strategies.
+    ''',
+    long_description=read('README.md'),
+    author='Jonathan Gillett',
+    author_email='jonathan.gillett@uoit.ca',
+    url='https://bitbucket.org/gnu-user/mcsc-6030-projecct',
+    install_requires=[
+        'docopt',
+        'numpy',
+        'scipy',
+        'mpi4py'
+    ],
+    license=read('LICENSE'),
+    keywords='matrix multiplication, evolutionary strategies, parallelization'
+)
